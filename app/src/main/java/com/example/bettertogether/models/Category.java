efbe1a9ad6963ef41f5e287b6d4f328e68e542a2
Chibueze Ejiofor <chibuezeejiofor@fb.com>
2019-07-16 13:21:45 -0700

    Added award and category models.

--- app/src/main/java/com/example/bettertogether/models/Category.java
+++ app/src/main/java/com/example/bettertogether/models/Category.java
@@ -1,8 +1,40 @@
 package com.example.bettertogether.models;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
 
 @ParseClassName("Category")
 public class Category extends ParseObject {
+    public static final String NAME = "name";
+    public static final String GROUPS = "groups";
+    public static final String ICON = "icon";
+
+    public ParseFile getIcon() {
+        return getParseFile(ICON);
+    }
+
+    public void setIcon(ParseFile parseFile) {
+        put(ICON, parseFile);
+    }
+
+    public String getName() {
+        return getString(NAME);
+    }
+
+    public void setName(String name) {
+        put(NAME, name);
+    }
+
+    public ParseRelation<ParseObject> getGroups() {
+        return getRelation(GROUPS);
+
+    }
+
+    public void setGroups(ParseRelation groups) {
+        put(GROUPS, groups);
+    }
+
 }
