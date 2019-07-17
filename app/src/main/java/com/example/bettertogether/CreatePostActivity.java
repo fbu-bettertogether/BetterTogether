83a4bbd2f3f4b87fedb1eeaf404fd6d7dee490b7
Chibueze Ejiofor <chibuezeejiofor@fb.com>
2019-07-17 16:11:29 -0700

    Implemented the post creation activity and the tag activity. The tags still aren't being saved properly.

--- app/src/main/java/com/example/bettertogether/CreatePostActivity.java
+++ app/src/main/java/com/example/bettertogether/CreatePostActivity.java
@@ -12,36 +12,40 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.bettertogether.models.Group;
 import com.example.bettertogether.models.Post;
 import com.google.android.material.textfield.TextInputEditText;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class CreatePostActivity extends AppCompatActivity {
-
+    private final int TAG_REQUEST_CODE = 20;
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
     public final static int PROFILE_IMAGE_ACTIVITY_REQUEST_CODE = 583;
     public static final int RESULT_OK = -1;
     public final String APP_TAG = "BetterTogether";
+    ParseRelation<ParseUser> relation;
 
     private String photoFileName = "photo.jpg";
     private File photoFile;
@@ -56,7 +60,7 @@ public class CreatePostActivity extends AppCompatActivity {
     private TextView tvGroup;
     private TextInputEditText etPost;
     private Toolbar toolbar;
-    private ParseRelation<ParseUser> taggedUsers;
+    private ArrayList<ParseUser> taggedUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,13 +79,26 @@ public class CreatePostActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
+        // The Group needs to be passed to this activity through an intent
         group = getIntent().getParcelableExtra("group");
 
         Glide.with(this).load(ParseUser.getCurrentUser().getParseFile("profileImage").getUrl()).apply(RequestOptions.circleCropTransform()).into(ivIcon);
         tvUsername.setText(ParseUser.getCurrentUser().getUsername());
         tvGroup.setText(String.format("Post in %s", group.getName()));
-
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLaunchCamera(view);
+            }
+        });
+        ivTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreatePostActivity.this, TagActivity.class);
+                intent.putExtra("group", group);
+                startActivityForResult(intent, TAG_REQUEST_CODE);
+            }
+        });
     }
 
     @Override
@@ -108,7 +125,12 @@ public class CreatePostActivity extends AppCompatActivity {
             post.setMedia(new ParseFile(photoFile));
         }
         if (taggedUsers != null) {
-            post.setTaggedUsers(taggedUsers);
+            relation = post.getRelation("taggedUsers");
+            for (int i = 0; i < taggedUsers.size(); i++) {
+                relation.add(taggedUsers.get(i));
+            }
+            // Tags are not being saved properly
+            post.setTaggedUsers(relation);
         }
         post.setDescription(etPost.getText().toString());
         post.setUser(ParseUser.getCurrentUser());
@@ -170,6 +192,17 @@ public class CreatePostActivity extends AppCompatActivity {
             if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
                 Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
                 ivCamera.setImageBitmap(takenImage);
+            } else if (requestCode == TAG_REQUEST_CODE) {
+                taggedUsers = data.getParcelableArrayListExtra("taggedUsers");
+                String tagText = "";
+                if (taggedUsers == null) {
+                    taggedUsers = new ArrayList<>();
+                }
+                for (int i = 0; i < taggedUsers.size(); i++) {
+                    tagText = tagText + "@" + taggedUsers.get(i).getUsername() + ",";
+                }
+                tagText = tagText.substring(0, tagText.length() - 1);
+                tvTag.setText(tagText);
             }
         }
     }
---
+++ app/src/main/java/com/example/bettertogether/TagActivity.java
@@ -0,0 +1,92 @@
+package com.example.bettertogether;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.bettertogether.models.Group;
+import com.example.bettertogether.models.Post;
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TagActivity extends AppCompatActivity {
+    private RecyclerView rvMembers;
+    private MemberAdapter adapter;
+    private Toolbar toolbar;
+
+    private Group group;
+    private ArrayList<ParseUser> taggedUsers;
+    private ArrayList<ParseUser> groupMembers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag);
+        toolbar = findViewById(R.id.toolbar);
+        rvMembers = findViewById(R.id.rvMembers);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        group = getIntent().getParcelableExtra("group");
+        taggedUsers = new ArrayList<>();
+        groupMembers = new ArrayList<>();
+        adapter = new MemberAdapter(groupMembers, taggedUsers);
+        rvMembers.setLayoutManager(new LinearLayoutManager(rvMembers.getContext()));
+        rvMembers.setAdapter(adapter);
+
+        ParseQuery<ParseUser> groupQuery = group.getUsers().getQuery();
+        groupQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e != null) {
+                    Log.e("Querying groups", "error with query");
+                    e.printStackTrace();
+                }
+                groupMembers.addAll(objects);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.compose_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menuCompose:
+            default:
+                Intent intent = new Intent();
+                intent.putParcelableArrayListExtra("taggedUsers", taggedUsers);
+                setResult(RESULT_OK, intent); // set result code and bundle data for response
+                finish(); // closes the activity, pass data to parent
+                return true;
+        }
+    }
+
+}
